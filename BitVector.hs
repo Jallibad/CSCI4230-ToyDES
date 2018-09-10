79cf24180f5e6893712213c3eb30d5ec4b120440
Jordan <jallibad@gmail.com>
2018-09-10 09:31:23 -0400

    Reorganizing and documenting

--- BitVector.hs
+++ BitVector.hs
@@ -1,68 +1,118 @@
-{-# LANGUAGE KindSignatures, ConstraintKinds, RankNTypes, FlexibleContexts, TypeOperators, TypeFamilies, FlexibleInstances, UndecidableInstances #-}
+{-# LANGUAGE ConstraintKinds		#-}
+{-# LANGUAGE DataKinds				#-}
+{-# LANGUAGE FlexibleContexts		#-}
+{-# LANGUAGE KindSignatures			#-}
+{-# LANGUAGE RankNTypes				#-}
+{-# LANGUAGE ScopedTypeVariables	#-}
+{-# LANGUAGE TypeApplications		#-}
+{-# LANGUAGE TypeFamilies			#-}
+{-# LANGUAGE TypeOperators			#-}
+{-# LANGUAGE UndecidableInstances	#-}
+{-# LANGUAGE ViewPatterns			#-}
 
-module BitVector where
+module BitVector (
+	-- * Types
+	BitVector,
+	Thing,
+	Addition,
+	-- * Functions
+	-- ** Construction
+	-- ** Lenses
+	asVec,
+	asIntegral,
+	asList,
+	digitWise,
+	-- ** Size Modifying Functions
+	BitVector.concat,
+	BitVector.concat',
+	split,
+	-- ** Miscellaneous
+	backPermute,
+	backPermute'
+)where
 
 import Control.Arrow ((***))
-import Data.Ratio
-import Data.Char (intToDigit)
-import Data.Ord (comparing)
+import Control.Lens
 import Data.Bits
-import Data.Vector.Fixed as Vec hiding (map, fromList)
-import qualified Data.Vector.Fixed as Vec (map, fromList)
-import Data.Vector.Fixed.Boxed hiding (map, fromList)
-import Data.Vector.Fixed.Cont hiding (map, fromList)
+import Data.Char (intToDigit)
 import Data.Function (on)
-import Data.Maybe
+import Data.Monoid (Sum)
+import Data.Ord (comparing)
+import Data.Proxy
+import Data.Ratio ((%))
+import Data.Vector.Fixed.Unboxed (Vec)
+import Data.Vector.Fixed.Cont hiding (map, fromList, toList)
+import Data.Vector.Fixed as Vec hiding (map, fromList, toList)
+import qualified Data.Vector.Fixed as Vec (map, fromList, toList)
 import GHC.TypeLits
 import Numeric (showIntAtBase)
 
---data BitVector (n :: Nat) = BitVector (Vec n Bool)
-newtype BitVector (n :: Nat) = BitVector (Vec n Bool)
+-- |The basic datatype of the BitVector module, represents a vector of booleans with the static size `n`
+newtype BitVector (n :: Nat) = BitVector {_vec :: Vec n Bool}
 
 type Thing a = (Peano (a + 1) ~ S (Peano a), KnownNat a, ArityPeano (Peano a))
+type Addition a b = Peano (a+b) ~ Add (Peano a) (Peano b)
+
+-- |Lens that converts the 'BitVector' to a 'Data.Vector.Fixed.Unboxed.Vec' n 'Bool'.
+asVec :: Lens' (BitVector n) (Vec n Bool)
+asVec = lens unwrap $ const BitVector
+
+-- |Lens that converts the 'BitVector' to any 'Integral' a
+asIntegral :: (Integral a, Thing n) => Lens' (BitVector n) a
+asIntegral = lens fromIntegral $ const fromIntegral
+
+asList :: Thing n => Lens' (BitVector n) [Bool]
+asList = lens toList $ const fromList
+
+digitWise :: Thing n => Setter' (BitVector n) Bool
+digitWise = setting $ (asVec %~) . Vec.map
+
+--digitsFold :: Thing n => Fold (BitVector n) Bool
+--digitsFold = folding unwrap
 
 unwrap :: BitVector n -> Vec n Bool
 unwrap (BitVector v) = v
 
+using :: (a -> a -> a) -> Lens' (BitVector n) a -> BitVector n -> BitVector n -> BitVector n
+using f l = over l . f . view l
+
 instance Thing a => Eq (BitVector a) where
-	(==) = (==) `on` unwrap
+	(==) = (==) `on` (^. asVec)
 
 instance Thing a => Show (BitVector a) where
-	show = ("0b"++) . Vec.foldr (\b s -> (if b then '1' else '0') : s) "" . unwrap
+	show = ("0b"++) . Prelude.map (\b -> if b then '1' else '0') . BitVector.toList
 
 instance Thing a => Num (BitVector a) where
-	(+) = (fromInteger .) . on (+) toInteger
-	(*) = (fromInteger .) . on (*) toInteger
-	(-) = (fromInteger .) . on (-) toInteger
-	abs = fromInteger . abs . toInteger
+	(+) = (+) `using` asIntegral
+	(*) = (*) `using` asIntegral
+	(-) = (-) `using` asIntegral
+	abs = id
 	signum _ = 1
 	negate = error "Negation is not defined for boolean vectors"
 	fromInteger = BitVector . Vec.reverse . Vec.generate . testBit
 
-instance (Thing a, Eq (BitVector a)) => Bits (BitVector a) where
-	(.&.) = (BitVector .) . (Vec.zipWith (.&.) `on` unwrap)
-	(.|.) = (BitVector .) . (Vec.zipWith (.|.) `on` unwrap)
-	xor = (BitVector .) . (Vec.zipWith xor `on` unwrap)
-	complement = BitVector . Vec.map not . unwrap
-	shift (BitVector b) i = fromList $ map mapFunction [0.. len-1]
-		where
-			mapFunction n = (n+i >= 0 && n+i < len) && b Vec.! (n+i)
-			len = Vec.length b
-	--rotate (BitVector b) i = Prelude.map (`mod` len) 
-	--	where
-	--		len = Vec.length b
+instance Thing a => Bits (BitVector a) where
+	(.&.) = Vec.zipWith (.&.) `using` asVec
+	(.|.) = Vec.zipWith (.|.) `using` asVec
+	xor = Vec.zipWith xor `using` asVec
+	complement = digitWise %~ not
+	shift b i = thing (\((+i) -> n) -> n >= 0 && n < BitVector.length b && testBit b n) b
+	rotate b i = fromList $ map
+					(\n -> testBit b $ (n+i) `mod` len)
+					[0.. len-1]
+		where len = BitVector.length b
 	bit = bitDefault
-	testBit (BitVector b) i = b Vec.! i
-	bitSizeMaybe = Just . Vec.length . unwrap
-	bitSize = Vec.length . unwrap
+	testBit = (Vec.!) . (^. asVec)
+	bitSizeMaybe = Just . BitVector.length
+	bitSize = BitVector.length
 	isSigned _ = False
 	popCount = popCountDefault
 
-instance (Thing a, Real (BitVector a), Enum (BitVector a)) => Integral (BitVector a) where
+instance Thing a => Integral (BitVector a) where
 	toInteger = Vec.foldl (\n d -> 2*n + if d then 1 else 0) 0 . unwrap
 	quotRem = ((fromInteger *** fromInteger) .) . on quotRem fromIntegral
 
-instance (Thing a, Enum (BitVector a), Ord (BitVector a)) => Real (BitVector a) where
+instance Thing a => Real (BitVector a) where
 	toRational = (%1) . toInteger
 
 instance Thing a => Enum (BitVector a) where
@@ -72,17 +122,33 @@ instance Thing a => Enum (BitVector a) where
 instance Thing a => Ord (BitVector a) where
 	compare = comparing toInteger
 
-bitVectorSplit :: (Thing a, Thing b, Peano (b+b) ~ Add (Peano b) (Peano b), (b+b) ~ a) => BitVector a -> (BitVector b, BitVector b)
-bitVectorSplit (BitVector v) = (fromList *** fromList) $ splitAt (Vec.length v `div` 2) $ Vec.toList v
+length :: forall b. KnownNat b => BitVector b -> Int
+length = const $ fromIntegral $ natVal $ Proxy @b
 
 fromList :: Thing a => [Bool] -> BitVector a
 fromList = BitVector . Vec.fromList
 
 toList :: Thing a => BitVector a -> [Bool]
-toList = Vec.toList . unwrap
+toList = Vec.toList . (^. asVec)
 
-concat :: (Thing v, Thing w, Peano (v+v) ~ Add (Peano v) (Peano v), (v+v) ~ w) => BitVector v -> BitVector v -> BitVector w
-concat = (BitVector .) . on Vec.concat unwrap
+split :: forall a b c. (Thing a, Thing b, Thing c, a ~ (b+c)) => BitVector a -> (BitVector b, BitVector c)
+split =	(fromList *** fromList)
+		. splitAt (fromIntegral $ natVal $ Proxy @b)
+		. BitVector.toList
 
-backPermute :: (Thing a, Thing b) => [Int] -> BitVector a -> BitVector b
-backPermute is b = fromList $ Prelude.map (testBit b) is
+-- |Concatenates two 'BitVector's of size @a@ and @b@ resulting in a new 'BitVector' with size @a+b ~ c@
+concat :: (Thing a, Thing b, Thing c, Addition a b, (a+b) ~ c) => BitVector a -> BitVector b -> BitVector c
+concat a b = BitVector $ Vec.concat (a ^. asVec) (b ^. asVec)
+
+-- |Uncurried version of 'concat'
+concat' :: (Thing a, Thing b, Addition a a, (a+a) ~ b) => (BitVector a, BitVector a) -> BitVector b
+concat' = uncurry BitVector.concat
+
+backPermute :: (Thing a, Thing b) => Vec b Int -> BitVector a -> BitVector b
+backPermute is = BitVector . flip Vec.map is . testBit
+
+backPermute' :: (Thing a, Thing b) => [Int] -> BitVector a -> BitVector b
+backPermute' is = fromList . flip Prelude.map is . testBit
+
+thing :: (Thing a, Thing b) => (Int -> Bool) -> BitVector a -> BitVector b
+thing f b = fromList $ map f [0.. BitVector.length b - 1]
--- ToyDES.hs
+++ ToyDES.hs
@@ -1,25 +1,46 @@
-{-# LANGUAGE BinaryLiterals, FlexibleContexts, TypeFamilies #-}
+{-# LANGUAGE BinaryLiterals		#-}
+{-# LANGUAGE DataKinds			#-}
+{-# LANGUAGE FlexibleContexts	#-}
+{-# LANGUAGE TypeApplications	#-}
+{-# LANGUAGE TypeFamilies		#-}
+{-# LANGUAGE TypeOperators		#-}
 
-import Control.Arrow ((***))
-import Data.List (tails, genericIndex)
-import Data.Bits (xor, testBit, shift)
 import BitVector
+import Control.Arrow ((***))
+import Control.Lens ((%~), both)
+import Data.Bits (xor, testBit, shift)
+import Data.List (tails, genericIndex)
+import Data.Tuple (swap)
+import GHC.TypeLits
 
 expansion :: BitVector 6 -> BitVector 8
-expansion = backPermute [0,1,3,2,3,2,4,5]
+expansion = backPermute' [0,1,3,2,3,2,4,5]
 
 expansion' :: BitVector 4 -> BitVector 8
-expansion' = backPermute [4,1,2,3,2,3,4,1]
+expansion' = backPermute' [4,1,2,3,2,3,4,1]
 
 f :: BitVector 6 -> BitVector 8 -> BitVector 6
-f r k = uncurry BitVector.concat	-- Apply permutation box
-		$ (s1 *** s2)				-- Apply the substitution boxes
-		$ (bitVectorSplit			:: BitVector 8 -> (BitVector 4, BitVector 4))
-		$ expansion r `xor` k		--
+f r k = BitVector.concat'		-- Apply permutation box
+		$ s1 *** s2				-- Apply the substitution boxes
+		$ BitVector.split		--
+		$ expansion r `xor` k	--
 	where
-		box = genericIndex			:: [BitVector 3] -> BitVector 4 -> BitVector 3
+		box :: [BitVector 3] -> BitVector 4 -> BitVector 3
+		box = genericIndex
 		s1 = box [0b101,0b010,0b001,0b110,0b011,0b100,0b111,0b000,0b001,0b100,0b110,0b010,0b000,0b111,0b101,0b011]
 		s2 = box [0b100,0b000,0b110,0b101,0b111,0b001,0b011,0b010,0b101,0b011,0b000,0b111,0b110,0b010,0b001,0b100]
+
+f' :: BitVector 4 -> BitVector 8 -> BitVector 4
+f' r =	backPermute' [1,3,2,0]	-- Apply P4 permutation
+		. BitVector.concat'		-- Concat the output of the s-boxes
+		. (s0 *** s1)			-- Use the s-boxes
+		. BitVector.split		-- Split the result into s-box inputs
+		. xor (expansion' r)	-- Expand block then XOR with key
+	where
+		box :: [[BitVector 2]] -> BitVector 4 -> BitVector 2
+		box s i = error "Undefined"	-- TODO Define
+		s0 = box [[1,0,3,2],[3,2,1,0],[0,2,1,3],[3,1,3,2]]
+		s1 = box [[0,1,2,3],[2,0,1,3],[3,0,1,0],[2,1,0,3]]
 
 makeKeys :: Thing keylength => BitVector keylength -> [BitVector 8]
 makeKeys =	take 2						--
@@ -27,27 +48,33 @@ makeKeys =	take 2						--
 			. tails . cycle . toList	--
 
 makeKeys' :: BitVector 10 -> [BitVector 8]
-makeKeys' = take 2 . tail							-- Take the result of applying the operations 1x and 2x
-			. map (p8 . combineKeyHalves)			-- Use the provided p8 permutation to make key from halves
-			. iterate (leftShift *** leftShift)		-- Repeatedly left shift both key halves
-			. bitVectorSplit . p10					-- Apply initial p10 permutation and then split key in half
+makeKeys' = take 2 . tail						-- Take the result of applying the operations 1x and 2x
+			. map (p8 . combineKeyHalves)		-- Use the provided p8 permutation to make key from halves
+			. iterate (both %~ leftShift)		-- Repeatedly left shift both key halves
+			. BitVector.split . p10				-- Apply initial p10 permutation and then split key in half
 	where
-		combineKeyHalves = uncurry BitVector.concat :: (BitVector 5, BitVector 5) -> BitVector 10
+		combineKeyHalves :: (BitVector 5, BitVector 5) -> BitVector 10
+		combineKeyHalves = BitVector.concat'
 		leftShift = flip shift 1
-
-p10 :: BitVector 10 -> BitVector 10
-p10 = backPermute [2,4,1,6,3,9,0,8,7,5]
-
-p8 :: BitVector 10 -> BitVector 8
-p8 = backPermute [5,2,6,3,7,4,9,8]
+		p10 = backPermute' @10 @10 [2,4,1,6,3,9,0,8,7,5]
+		p8 = backPermute' @10 @8 [5,2,6,3,7,4,9,8]
 
 encrypt :: Thing keylength => BitVector 12 -> BitVector keylength -> BitVector 12
-encrypt p = uncurry (flip BitVector.concat)	-- Flip and recombine l and r
-			. foldl feistel					-- Apply the feistel function for each of the keys
-				(bitVectorSplit p)			-- Initial value to fold: plaintext block split in half
-			. makeKeys						-- Returns a rotating list of keys [0..length of DES cycle]
+encrypt p = BitVector.concat' . swap	-- Flip and recombine l and r
+			. foldl feistel				-- Apply the feistel function for each of the keys
+				(BitVector.split p)		-- Initial value to fold: plaintext block split in half
+			. makeKeys					-- Returns a rotating list of keys [0..length of DES cycle]
+
+encrypt' :: Thing keylength => BitVector 12 -> BitVector keylength -> BitVector 12
+encrypt' p = BitVector.concat' . swap	-- Flip and recombine l and r
+			. foldl feistel				-- Apply the feistel function for each of the keys
+				(BitVector.split p)		-- Initial value to fold: plaintext block split in half
+			. makeKeys					-- Returns a rotating list of keys [0..length of DES cycle]
 
 feistel :: (BitVector 6, BitVector 6) -> BitVector 8 -> (BitVector 6, BitVector 6)
-feistel (l,r) k = (r, l `xor` (f r k))
+feistel (l,r) k = (r, l `xor` f r k)
+
+feistel' :: (Thing p, Thing k, (p+p) ~ k) => (BitVector p -> BitVector k -> BitVector p) -> (BitVector p, BitVector p) -> BitVector k -> (BitVector p, BitVector p)
+feistel' f (l,r) k = (r, l `xor` f r k)
 
 main = print $ encrypt 0b100010110101 (0b111000111 :: BitVector 9)
